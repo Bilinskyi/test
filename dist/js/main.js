082d4531768d22c29bca3243c683381ca5dc69f9
Taras Bilinskyi <1unitedcrew@gmail.com>
2016-09-22 12:40:52 +0300

    v3

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,69 +1,34 @@
 
 $(document).ready(function(){
 
-$(".owl-carousel").owlCarousel({
- navigation : true,
-    navigationText : ["",""],
-    rewindNav : true,
-    scrollPerPage : false, 
-    singleItem : true
+  $(".owl-carousel").owlCarousel({
+   navigation : true,
+   navigationText : ["",""],
+   rewindNav : true,
+   scrollPerPage : false, 
+   singleItem : true
 
-  });
+ });
 
-// $('#slider-1').bxSlider({
-// 		controls: true, 
-// 		pager: false,
-// 		slideWidth: 940,
-// 		minSlides: 4,
-// 		maxSlides: 4,
-// 		moveSlides: 1
-// 	});
+  $('.more-link').on('click', function(e) {
+   e.preventDefault(); 
+   $(this).hide().prevAll().css('display', 'inline-block');
+ });
 
-// $("#owl-demo").owlCarousel({
-//       navigation : true, // Show next and prev buttons
-//       slideSpeed : 300,
-//       paginationSpeed : 400,
-//       singleItem: false,
-//       items: 4,
-//       pagination : true,
-//       itemsDesktop : [1920,4],
-//       navigationText: false,
-//       // autoPlay: 20000,
-//       // "singleItem:true" is a shortcut for:
-//       // items : 1, 
-//       // itemsDesktop : false,
-//       // itemsDesktopSmall : false,
-//       // itemsTablet: false,
-//       // itemsMobile : false
-//     mouseDrag : false,
-//     touchDrag : false
+  $('.menu-btn').on('click', function(e) {
+   e.preventDefault(); 
+   $(this).closest('header').find('.header-menu').slideToggle();
+ });
 
-//   });  
+  $('.drop-language').on('click', function(e) {
+   e.preventDefault(); 
+   $(this).next().slideToggle();
+ });
 
- //    $("a.modal-form").fancybox({
- //       'hideOnContentClick': true,
- //          minWidth : 230,
- //          padding : 0,
- //          closeBtn : true
- 
- // });
+  $('.head-2').on('click', function(e) {
+   e.preventDefault(); 
+   $(this).next().slideToggle();
+ });
 
-// $('header a[href^="#"]').on('click', function(event) {
-
-//     var target = $( $(this).attr('href') );
-
-//     if( target.length ) {
-//         event.preventDefault();
-//         $('html, body').animate({
-//             scrollTop: target.offset().top -85
-//         }, 600);
-//     }
-
-// });
-
-//  jQuery(function($){ 
-//    $(".phone_mask_1").mask("+7(999) 999-9999");
-//    $("#phone_mask_2").mask("+7(999) 999-9999");
-// });
 
 });
